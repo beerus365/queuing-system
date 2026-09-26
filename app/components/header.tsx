dbf702b5a5c18ff2b709c0a21546701863cee139
beerus <ajdbriones01202401029@usep.edu.ph>
2026-09-26 11:09:25 +0800

    change system name

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,8 +6,8 @@ const Header = async () => {
     return (
         <header className="fixed inset-x-0 top-0 z-60 flex flex-col gap-4 bg-foreground px-4 py-4 text-white sm:flex-row sm:items-center sm:justify-between sm:px-6 sm:py-8">
             <div className="flex min-w-0 flex-col items-start">
-                <p className="text-xs sm:text-sm">That's My QueueMboy</p>
-                <h1 className="text-lg font-bold sm:text-2xl">TMQ - Queuing System</h1>
+                <p className="text-xs sm:text-sm">synQ</p>
+                <h1 className="text-lg font-bold sm:text-2xl">synQ - Queuing System</h1>
             </div>
 
             <div className="flex flex-col items-start sm:items-end">
